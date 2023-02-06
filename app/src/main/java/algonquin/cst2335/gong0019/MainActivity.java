03d72f8ceb729371c8dc9e4497df5354a019b1d6
Yi Gong <gong0019@algonquinlive.com>
2023-02-06 14:02:16 -0500

    Finished Phone Version of Lab3

--- app/src/main/java/algonquin/cst2335/gong0019/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/gong0019/MainActivity.java
@@ -1,14 +1,39 @@
 package algonquin.cst2335.gong0019;
 
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.switch1);
+
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            } else {
+                imgView.clearAnimation();
+
+            }
+        });
     }
 }
