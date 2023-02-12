601152d8494c5aa37853af58d29d89000a3a97ec
Yi Gong <gong0019@algonquinlive.com>
2023-02-11 23:02:40 -0500

    Finished Lab4

--- app/src/main/java/algonquin/cst2335/gong0019/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/gong0019/MainActivity.java
@@ -1,32 +1,44 @@
 package algonquin.cst2335.gong0019;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-private static String TAG = "MainActivity";
-
+    private static String TAG = "MainActivity";
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override //Application starts
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
 
-        Button loginButton = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
 
         loginButton.setOnClickListener(clk -> {
             Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
 
-            nextPage.putExtra("Email","123@gmail.com");
+            nextPage.putExtra("Email",emailEditText.getText().toString());
 
-            nextPage.putExtra("Password","123456");
+            nextPage.putExtra("Password",passwordEditText.getText().toString());
+            SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString("Email", emailEditText.getText().toString());
+            editor.putString("Password", passwordEditText.getText().toString());
+            editor.apply();
 
             startActivity(nextPage);
         });
@@ -35,6 +47,7 @@ private static String TAG = "MainActivity";
     @Override // Activity is now visible
     protected void onStart() {
         super.onStart();
+
 
         Log.w(TAG, "Now visible");
 
--- app/src/main/java/algonquin/cst2335/gong0019/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/gong0019/SecondActivity.java
@@ -1,15 +1,15 @@
 package algonquin.cst2335.gong0019;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,55 +17,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import algonquin.cst2335.gong0019.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent dataFromPage1 = getIntent();
-        String email = dataFromPage1.getStringExtra("Email");
-        String passwordString = dataFromPage1.getStringExtra("Password");
-        int password = Integer.parseInt(passwordString);
-        Log.d("Second activity", "Email is" + email + "and password is" + password);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String email = sharedPreferences.getString("Email", "");
+        String password = sharedPreferences.getString("Password", "");
+
+        Log.d("SecondActivity", "Email: " + email + " Password: " + password);
 
         ActivitySecondBinding binding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-binding.button.setOnClickListener( clk -> {
-    finish();
-});
 
         binding.secondPageButton.setOnClickListener(clk -> {
 
             Intent call = new Intent(Intent.ACTION_DIAL);
             call.setData(Uri.parse("tel:8193297373"));
             startActivity(call);
-
         });
 
-        binding.imageView.setOnClickListener(clk -> {
-
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivity(cameraIntent);
-
-        });
         ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            Bitmap thumbnail = data.getParcelableExtra("data");
-
-                        }
-
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        binding.imageView.setImageBitmap(imageBitmap);
                     }
-
                 });
 
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        binding.imageView.setOnClickListener(clk -> {
-            cameraResult.launch(cameraIntent);
+        binding.imageView.setOnClickListener(clk -> cameraResult.launch(cameraIntent));
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
         });
     }
 }
+
+
